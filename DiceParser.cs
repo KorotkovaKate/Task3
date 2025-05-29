fd2bb643a5ca696846b177d516b523cc58b3e8ab
KorotkovaKate <korotkovakate10@gmail.com>
2025-05-29 22:28:35 +0300

    Create DiceParser

---
+++ DiceParser.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    public class DiceParser
+    {
+        public static List<List<int>> Parse(string[] args)
+        {
+            if (args.Length < 3)
+            {
+                Console.WriteLine("You must provide at least 3 dice configurations");
+                Environment.Exit(0);
+            }
+            var dices = new List<List<int>>();
+            var faces = new List<int>();
+            foreach (var arg in args)
+            {
+                string[] values = arg.Split(',');
+                if (values.Length < 2)
+                {
+                    Console.WriteLine($"Invalid dice configuration: {arg}. Each dice must have at least two faces");
+                    Environment.Exit(0);
+                }
+                foreach (var value in values)
+                {
+                    if (int.TryParse(value, out int face))
+                    {
+                        faces.Add(face);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Invalid dice value: {value} in {arg}. It must be only integers");
+                        Environment.Exit(0);
+                    }
+                }
+                dices.Add(faces);
+            }
+            return dices;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,11 +1,21 @@
 ﻿using System;
-using System.Diagnostics.Contracts;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
-class Program
+namespace Task3
 {
-    static void Main(string[] args)
+    public class Program
     {
-
+        static void Main(string[] args)
+        {
+            string[] values = ["1,2,3,4,5,6", "1,2,3,4,5,6", "1,2,3,4,5,6"];
+            var dices = DiceParser.Parse(values);
+            foreach (var arg in values)
+            {
+                Console.WriteLine(arg);
+            }
+        }
     }
 }
