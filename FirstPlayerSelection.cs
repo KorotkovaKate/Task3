737833a2a17333b855fd033a641945d48cc51861
KorotkovaKate <korotkovakate10@gmail.com>
2025-05-30 17:53:26 +0300

    Create class FirstPlayerSelection and full it

---
+++ FirstPlayerSelection.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    public class FirstPlayerSelection
+    {
+        public static bool SelectFirstMove()
+        {
+            byte[] randoms = RandomGenerator.RandomGeneratorBytes();
+            string key = Encrypter.CreateKey(randoms);
+            int computerSelection = RandomGenerator.ChooseRandomNumber(0, 1);
+            string hmac = Encrypter.CreateHMAC(key, computerSelection);
+            Console.WriteLine($"Let's determine who makes the first move");
+            Console.WriteLine($"I selected a random value in the range 0..1 (HMAC={hmac}). Try to guess my selection");
+            Console.WriteLine("0 - 0\n1 - 1\nX - exit\n? - help");
+            bool isGuess;
+            while (true)
+            {
+                Console.Write("Your selection: ");
+                string input = Console.ReadLine();
+                if (input == "X") Environment.Exit(0);
+                if (input == "?") Console.WriteLine("Table");//To do later
+                if (input == "0" || input == "1")
+                {
+                    Console.WriteLine($"My selection: {computerSelection} (KEY={key})");
+                    if (input == computerSelection.ToString()) isGuess = true;
+                    else isGuess = false;
+                    return isGuess;
+                }
+                Console.WriteLine("Invalid input. Please choose 0 or 1");
+            }
+        }
+    }
+}
