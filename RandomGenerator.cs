5c2f3018bf20ba53dc4554320d0ab51b541c5c2e
KorotkovaKate <korotkovakate10@gmail.com>
2025-05-30 14:48:23 +0300

    Create RandomGenerator and full it

---
+++ RandomGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    public class RandomGenerator
+    {
+        public static byte[] RandomGeneratorBytes()
+        {
+            byte[] randomBytes = new byte[32];
+            RandomNumberGenerator.Fill(randomBytes);
+            return randomBytes;
+        }
+        public static int ChooseRandomNumber(int min, int max)
+        {
+            return RandomNumberGenerator.GetInt32(min, max + 1);
+        }
+    }
+}
