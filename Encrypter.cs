aff8cd2aea86b7b3dc6c9975fcec6c84e81d26d7
KorotkovaKate <korotkovakate10@gmail.com>
2025-05-30 14:50:01 +0300

    Install SHA3.Net, create class Encrypter and full it

---
+++ Encrypter.cs
@@ -0,0 +1,27 @@
+﻿using SHA3.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    public class Encrypter
+    {
+        public string CreateKey(byte[] keyBytes)
+        {
+            byte[] hashedBytes = Sha3.Sha3256().ComputeHash(keyBytes);
+            string key = BitConverter.ToString(hashedBytes).Replace("-", "").ToLower();
+            return key;
+        }
+        public string CreateHMAC(string key, int number)
+        {
+            byte[] combinedBytes = Encoding.UTF8.GetBytes(key + number);
+            byte[] hashedBytes = Sha3.Sha3256().ComputeHash(combinedBytes);
+            string hmac = BitConverter.ToString(hashedBytes).Replace("-", "").ToLower();
+            return hmac;
+        }
+    }
+}
