8bf5a3f58c883d47710f4dc160d5aed62b78d48d
KorotkovaKate <korotkovakate10@gmail.com>
2025-05-30 05:21:13 +0300

    Create class Checker and bringing all checks here from DiceParser

---
+++ Checker.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    public class Checker
+    {
+        public static void CheckDiceCount(int count) 
+        {
+            if (count < 3)
+            {
+                Console.WriteLine("You must provide at least 3 dice configurations");
+                Environment.Exit(0);
+            }
+        }
+        public static void CheckDiceFaces(string arg, string[] values)
+        {
+            if (values.Length < 2)
+            {
+                Console.WriteLine($"Invalid dice configuration {arg} Each dice must have at least two faces");
+                Environment.Exit(0);
+            }
+            foreach (var value in values)
+            {
+                if (!int.TryParse(value, out _))
+                {
+                    Console.WriteLine($"Invalid dice value {value} in {arg} It must be only integers");
+                    Environment.Exit(0);
+                }
+            }
+        }
+    }
+}
--- DiceParser.cs
+++ DiceParser.cs
@@ -10,32 +10,16 @@ namespace Task3
     {
         public static List<List<int>> Parse(string[] args)
         {
-            if (args.Length < 3)
-            {
-                Console.WriteLine("You must provide at least 3 dice configurations");
-                Environment.Exit(0);
-            }
+            Checker.CheckDiceCount(args.Length);
             var dices = new List<List<int>>();
             var faces = new List<int>();
             foreach (var arg in args)
             {
                 string[] values = arg.Split(',');
-                if (values.Length < 2)
-                {
-                    Console.WriteLine($"Invalid dice configuration: {arg}. Each dice must have at least two faces");
-                    Environment.Exit(0);
-                }
+                Checker.CheckDiceFaces(arg, values);
                 foreach (var value in values)
                 {
-                    if (int.TryParse(value, out int face))
-                    {
-                        faces.Add(face);
-                    }
-                    else
-                    {
-                        Console.WriteLine($"Invalid dice value: {value} in {arg}. It must be only integers");
-                        Environment.Exit(0);
-                    }
+                        faces.Add(Convert.ToInt32(value));
                 }
                 dices.Add(faces);
             }
