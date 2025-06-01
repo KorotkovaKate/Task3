77bf8beae27bc331ec2c52f27794d3b973e95c44
KorotkovaKate <korotkovakate10@gmail.com>
2025-06-01 19:18:27 +0300

    renamed class ChooseDices

--- SelectionDices.cs
+++ SelectionDices.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Task3
 {
-    public class ChooseDices
+    public class SelectionDices
     {
         public static void OutputGeneralData(List<List<int>> dices)
         {
@@ -18,45 +18,57 @@ namespace Task3
             Console.WriteLine("X - exit\n? - help");
             Console.Write("Your selection: ");
         }
-        public static void UserSelect(string userSelection, List<List<int>> dices)
+        public static string UserSelect (List<List<int>> dices)
         {
             while (true) 
             {
+                string userSelection = Console.ReadLine();
                 if (userSelection == "X") Environment.Exit(0);
-                if (userSelection == "?") Console.WriteLine("Table");//To do later
-                if (!int.TryParse(userSelection, out int chosenIndex) || chosenIndex < 0 || chosenIndex >= dices.Count)
+                if (userSelection == "?")
+                {
+                    HelpTable.DisplayHelpTable(Program.dices);
+                    continue;
+                }
+                else if (!int.TryParse(userSelection, out int chosenIndex) || chosenIndex < 0 || chosenIndex >= dices.Count)
                 {
                     Console.WriteLine("Invalid choice, select a valid one");
                 }
-                else return;
+                else return userSelection;
             }
-
         }
-        public static void StartWithUser(List<List<int>> dices)
+        public static List<int> UserChooseDice(List<List<int>> dices)
+        {
+            string userSelection = UserSelect(dices);
+            List<int> userDice = dices[Convert.ToInt32(userSelection)];
+            return userDice;
+        }
+        public static List<int> ComputerChooseDice(List<List<int>> dices)
+        {
+            int randomIndex = RandomGenerator.ChooseRandomNumber(0, dices.Count - 1);
+            List<int> computerDice = dices[randomIndex];
+            return computerDice;
+        }
+        public static (List<int>, List<int>) StartWithUser(List<List<int>> dices)
         {
             OutputGeneralData(dices);
-            string userSelection = Console.ReadLine();
-            UserSelect(userSelection, dices);
-            List<int> userDice = dices[Convert.ToInt32(userSelection)];
+            List<int> userDice = UserChooseDice(dices);
             Console.WriteLine($"You choose the [{string.Join(",", userDice)}] dice");
             dices.Remove(userDice);
-            int randomIndex = RandomGenerator.ChooseRandomNumber(0, dices.Count - 1);
-            List<int> computerDice = dices[randomIndex];
+            List<int> computerDice = ComputerChooseDice(dices);
             Console.WriteLine($"I make my choice and select the [{string.Join(",", computerDice)}] dice");
             Console.WriteLine("It's time for your roll!");
+            return (userDice, computerDice);
         }
-        public static void StartWithComputer(List<List<int>> dices) 
+        public static (List<int>, List<int>) StartWithComputer(List<List<int>> dices) 
         {
-            int randomIndex = RandomGenerator.ChooseRandomNumber(0, dices.Count - 1);
-            List<int> computerDice = dices[randomIndex];
+            List<int> computerDice = ComputerChooseDice(dices);
             Console.WriteLine($"I make the first move and choose the [{string.Join(",", computerDice)}] dice");
             dices.Remove(computerDice);
             OutputGeneralData(dices);
-            string userSelection = Console.ReadLine();
-            UserSelect(userSelection, dices);
-            List<int> userDice = dices[Convert.ToInt32(userSelection)];
+            List<int> userDice = UserChooseDice(dices);
             Console.WriteLine($"You choose the [{string.Join(",", userDice)}] dice");
             Console.WriteLine("It's time for my roll!");
+            return (userDice, computerDice);
         }
     }
 }
